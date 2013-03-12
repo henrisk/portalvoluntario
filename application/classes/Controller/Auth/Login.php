8272d9d7f4560e33758e3028e752f091fc5b231e
henrisk <henrisk@gmail.com>
2013-03-12 13:46:52 -0300

    Login form.

--- application/classes/Controller/Auth/Login.php
+++ application/classes/Controller/Auth/Login.php
@@ -2,7 +2,36 @@
 
 class Controller_Auth_Login extends Controller {
 
+	/**
+	 * Principal view da página
+	 * @var VIEW
+	 */
+	private $view = null;
+	
+	/**
+	 * Método de acesso a página
+	 */
 	public function action_index() {
-		$this->response->body(View::factory('auth/login'));
+		$this->view = View::factory('auth/login');
+		$this->loadConfig();
+		
+		$this->response->body($this->view);
+	}
+	
+	/**
+	 * Carrega as configurações principais para a aplicação considerando que apenas essa página pode ser acessada sem autenticação
+	 */
+	private function loadConfig() {
+		$config = Kohana::$config->load('default.application');
+		$this->view->title = $config['title'];
+		$this->view->action = URL::site('auth_login/authenticate');
+	}
+	
+	/**
+	 * Realiza a lógica de autenticação, permitindo/barrando o acesso do usuário ao restante do sistema
+	 */
+	public function action_authenticate() {
+		//Implementar lógica de autenticação
+		$this->redirect('Auth_Login', 302);
 	}
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,1 +1,18 @@
-<h1>LOGIN</h1>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title><?php echo $title; ?></title>
+	</head>
+	<body>
+		<?php 
+			echo form::open($action, array('method' => 'post'));
+			echo 'Usuário:<br />';
+			echo form::input('txtUser');
+			echo '<br />Senha:<br />';
+			echo form::password('txtPassword');
+			echo '<br /><br />';
+			echo form::submit('btnLogin', 'Login');
+			echo form::close();
+		?>
+	</body>
+</html>
